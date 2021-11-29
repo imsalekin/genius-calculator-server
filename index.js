c9f6a6dea47c91c6dc03f00a67ebad22ccec863e
Nahid Salekin <36966165+nahidsalekin@users.noreply.github.com>
2021-11-30 00:56:45 +0600

    skipped env variables

--- index.js
+++ index.js
@@ -10,14 +10,15 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fiuyj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+//skipped using env variables here
+const uri = `mongodb+srv://mongodb02:VwsSjJNEcX1rQH5x@cluster0.fiuyj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
     try {
         await client.connect();
         const db = client.db('genius-calculator');
-        const resultsCollection = db.collection('results');
+        const resultsCollection = db.collection('results-demo');
 
         app.get('/results', async (req, res) => {
             const cursor = resultsCollection.find({});
